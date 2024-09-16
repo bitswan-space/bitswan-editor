193b5933925d5624593b21f31ec776bf3460f2ae
S1ro1 <matej.sirovatka@gmail.com>
2024-09-16 16:57:25 +0200

    more refactoring

--- Extension/src/extension.ts
+++ Extension/src/extension.ts
@@ -6,6 +6,48 @@ import axios from 'axios';
 import { getDeployDetails } from './deploy_details';
 import { BitswanPREViewProvider } from './views/bitswan_pre';
 
+async function _deployCommand() {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor || path.extname(editor.document.fileName) !== '.ipynb') {
+        vscode.window.showErrorMessage('Please open a Jupyter notebook before deploying.');
+        return;
+    }
+
+    const notebookPath = editor.document.uri.fsPath;
+    const details = await getDeployDetails(notebookPath);
+
+    if (!details) {
+        return;
+    }
+
+    try {
+        const deployUrl = new URL(path.join(details.deployUrl, "__jupyter-deploy-pipeline/"));
+        deployUrl.searchParams.append("secret", details.deploySecret);
+        deployUrl.searchParams.append("restart", "true");
+
+        vscode.window.showInformationMessage('Packing for deployment...');
+
+        const zip = new JSZip();
+        zip.file('main.ipynb', details.notebookJson);
+        const zipContents = await zip.generateAsync({ type: 'nodebuffer' });
+
+        vscode.window.showInformationMessage('Uploading to server...');
+
+        const response = await axios.post(deployUrl.toString(), zipContents, {
+            headers: { 'Content-Type': 'application/zip' },
+        });
+
+        if (response.status === 200) {
+            const status = response.data.status;
+            vscode.window.showInformationMessage(`Deployment status: ${status}`);
+        } else {
+            vscode.window.showErrorMessage('Deployment failed. Please try again.');
+        }
+    } catch (error: any) {
+        vscode.window.showErrorMessage(`Deployment error: ${error.message}`);
+    }
+}
+
 export function activate(context: vscode.ExtensionContext) {
     const provider = new BitswanPREViewProvider(context.extensionUri);
 
@@ -13,47 +55,7 @@ export function activate(context: vscode.ExtensionContext) {
         vscode.window.registerWebviewViewProvider(BitswanPREViewProvider.viewType, provider)
     );
 
-    let deployCommand = vscode.commands.registerCommand('extension.deployCurrentNotebook', async () => {
-        const editor = vscode.window.activeTextEditor;
-        if (!editor || path.extname(editor.document.fileName) !== '.ipynb') {
-            vscode.window.showErrorMessage('Please open a Jupyter notebook before deploying.');
-            return;
-        }
-
-        const notebookPath = editor.document.uri.fsPath;
-        const details = await getDeployDetails(notebookPath);
-
-        if (!details) {
-            return;
-        }
-
-        try {
-            const deployUrl = new URL(path.join(details.deployUrl, "__jupyter-deploy-pipeline/"));
-            deployUrl.searchParams.append("secret", details.deploySecret);
-            deployUrl.searchParams.append("restart", "true");
-
-            vscode.window.showInformationMessage('Packing for deployment...');
-
-            const zip = new JSZip();
-            zip.file('main.ipynb', details.notebookJson);
-            const zipContents = await zip.generateAsync({ type: 'nodebuffer' });
-
-            vscode.window.showInformationMessage('Uploading to server...');
-
-            const response = await axios.post(deployUrl.toString(), zipContents, {
-                headers: { 'Content-Type': 'application/zip' },
-            });
-
-            if (response.status === 200) {
-                const status = response.data.status;
-                vscode.window.showInformationMessage(`Deployment status: ${status}`);
-            } else {
-                vscode.window.showErrorMessage('Deployment failed. Please try again.');
-            }
-        } catch (error: any) {
-            vscode.window.showErrorMessage(`Deployment error: ${error.message}`);
-        }
-    });
+    let deployCommand = vscode.commands.registerCommand('extension.deployCurrentNotebook', _deployCommand);
 
     context.subscriptions.push(deployCommand);
 
