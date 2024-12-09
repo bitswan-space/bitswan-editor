611b9187a3622834e8c8e6bcca6124eae9254196
Lukas Vecerka <vecerkalukas18@gmail.com>
2024-12-09 09:47:51 +0100

    fix .bitswan copy location

--- update-entrypoint.sh
+++ update-entrypoint.sh
@@ -3,6 +3,6 @@ code-server --install-extension /tmp/extensions/bitswan-extension.vsix
 code-server --install-extension /tmp/extensions/github-copilot.vsix
 code-server --install-extension /tmp/extensions/pylance.vsix
 
-cp -r /tmp/.bitswan /home/coder/workspace/.bitswan
+cp -r /tmp/.bitswan /home/coder/workspace
 
 exec "$@"
