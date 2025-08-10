3fb2c89ce1819afe5f54a3cb84e033e63eb36dbf
Mike Farad <mike18farad@gmail.com>
2025-08-10 14:20:34 +0300

    refactor: undo formatting changes in lib.ts

--- Extension/src/lib.ts
+++ Extension/src/lib.ts
@@ -1,20 +1,13 @@
-import FormData from "form-data";
-import JSZip from "jszip";
-import { JupyterServerRequestResponse } from "./types";
-import { Readable } from "stream";
-import axios from "axios";
-import path from "path";
-import vscode from "vscode";
+import FormData from 'form-data';
+import JSZip from 'jszip';
+import { Readable } from 'stream';
+import axios from 'axios';
+import path from 'path';
+import vscode from 'vscode';
 
-export const zipDirectory = async (
-  dirPath: string,
-  relativePath: string = "",
-  zipFile: JSZip = new JSZip(),
-  outputChannel: vscode.OutputChannel
-) => {
-  const entries = await vscode.workspace.fs.readDirectory(
-    vscode.Uri.file(dirPath)
-  );
+export const zipDirectory = async (dirPath: string, relativePath: string = '', zipFile: JSZip = new JSZip(), outputChannel: vscode.OutputChannel) => {
+
+  const entries = await vscode.workspace.fs.readDirectory(vscode.Uri.file(dirPath));
   for (const [name, type] of entries) {
     const fullPath = path.join(dirPath, name);
     const zipPath = path.join(relativePath, name);
@@ -22,9 +15,7 @@ export const zipDirectory = async (
     if (type === vscode.FileType.Directory) {
       await zipDirectory(fullPath, zipPath, zipFile, outputChannel);
     } else {
-      const content = await vscode.workspace.fs.readFile(
-        vscode.Uri.file(fullPath)
-      );
+      const content = await vscode.workspace.fs.readFile(vscode.Uri.file(fullPath));
       outputChannel.appendLine(`Adding file ${fullPath}`);
       zipFile.file(zipPath, content);
     }
@@ -33,29 +24,28 @@ export const zipDirectory = async (
   return zipFile;
 };
 
+
 export const zip2stream = async (zipFile: JSZip) => {
   const stream = new Readable();
 
-  stream.push(await zipFile.generateAsync({ type: "nodebuffer" }));
+  stream.push(await zipFile.generateAsync({ type: 'nodebuffer' }));
   stream.push(null);
 
   return stream;
-};
 
-export const deploy = async (
-  deployUrl: string,
-  form: FormData,
-  secret: string
-) => {
+}
+
+
+export const deploy = async (deployUrl: string, form: FormData, secret: string) => {
   const response = await axios.post(deployUrl, form, {
     headers: {
-      "Content-Type": "multipart/form-data",
-      Authorization: `Bearer ${secret}`,
+      'Content-Type': 'multipart/form-data',
+      'Authorization': `Bearer ${secret}`
     },
   });
 
   return response.status == 200;
-};
+}
 
 export const activateDeployment = async (deployUrl: string, secret: string) => {
   const response = await axios.post(
@@ -63,17 +53,17 @@ export const activateDeployment = async (deployUrl: string, secret: string) => {
     {},
     {
       headers: {
-        Authorization: `Bearer ${secret}`,
+        'Authorization': `Bearer ${secret}`,
       },
     }
   );
 
   return response.status == 200;
-};
+}
 
 export const getAutomations = async (
   automationsUrl: string,
-  secret: string
+  secret: string,
 ) => {
   const response = await axios.get(automationsUrl, {
     headers: {
@@ -82,11 +72,9 @@ export const getAutomations = async (
   });
 
   if (response.status == 200) {
+
     if (!Array.isArray(response.data)) {
-      console.warn(
-        "[getAutomations] Unexpected response format:",
-        response.data
-      );
+      console.warn("[getAutomations] Unexpected response format:", response.data);
       return [];
     }
 
@@ -103,9 +91,10 @@ export const getAutomations = async (
 };
 
 export const getImages = async (imagesUrl: string, secret: string) => {
-  const response = await axios.get(imagesUrl, {
+  const response = await axios.get(
+    imagesUrl, {
     headers: {
-      Authorization: `Bearer ${secret}`,
+      'Authorization': `Bearer ${secret}`,
     },
   });
 
@@ -114,41 +103,35 @@ export const getImages = async (imagesUrl: string, secret: string) => {
   } else {
     throw new Error(`Failed to get images from GitOps`);
   }
-};
+}
 
-export const restartAutomation = async (
-  automationUrl: string,
-  secret: string
-) => {
+export const restartAutomation = async (automationUrl: string, secret: string) => {
   const response = await axios.post(
     automationUrl,
     {},
     {
       headers: {
-        Authorization: `Bearer ${secret}`,
+        'Authorization': `Bearer ${secret}`,
       },
     }
   );
 
   return response.status == 200;
-};
+}
 
-export const startAutomation = async (
-  automationUrl: string,
-  secret: string
-) => {
+export const startAutomation = async (automationUrl: string, secret: string) => {
   const response = await axios.post(
     automationUrl,
     {},
     {
       headers: {
-        Authorization: `Bearer ${secret}`,
+        'Authorization': `Bearer ${secret}`,
       },
     }
   );
 
   return response.status == 200;
-};
+}
 
 export const stopAutomation = async (automationUrl: string, secret: string) => {
   const response = await axios.post(
@@ -156,88 +139,68 @@ export const stopAutomation = async (automationUrl: string, secret: string) => {
     {},
     {
       headers: {
-        Authorization: `Bearer ${secret}`,
+        'Authorization': `Bearer ${secret}`,
       },
     }
   );
 
   return response.status == 200;
-};
+}
 
-export const getAutomationLogs = async (
-  automationUrl: string,
-  secret: string
-) => {
+export const getAutomationLogs = async (automationUrl: string, secret: string) => {
   const response = await axios.get(automationUrl, {
     headers: {
-      Authorization: `Bearer ${secret}`,
+      'Authorization': `Bearer ${secret}`,
     },
   });
 
   return response.data;
-};
+}
 
 export const getImageLogs = async (imageUrl: string, secret: string) => {
   const response = await axios.get(imageUrl, {
     headers: {
-      Authorization: `Bearer ${secret}`,
+      'Authorization': `Bearer ${secret}`,
     },
   });
 
   return response.data;
-};
+}
 
-export const activateAutomation = async (
-  automationUrl: string,
-  secret: string
-) => {
-  const response = await axios.post(
-    automationUrl,
-    {},
-    {
-      headers: {
-        Authorization: `Bearer ${secret}`,
-      },
-    }
-  );
-
-  return response.status == 200;
-};
-
-export const deactivateAutomation = async (
-  automationUrl: string,
-  secret: string
-) => {
-  const response = await axios.post(
-    automationUrl,
-    {},
-    {
-      headers: {
-        Authorization: `Bearer ${secret}`,
-      },
-    }
-  );
-
-  return response.status == 200;
-};
-
-export const deleteAutomation = async (
-  automationUrl: string,
-  secret: string
-) => {
-  const response = await axios.delete(automationUrl, {
+export const activateAutomation = async (automationUrl: string, secret: string) => {
+  const response = await axios.post(automationUrl, {}, {
     headers: {
-      Authorization: `Bearer ${secret}`,
+      'Authorization': `Bearer ${secret}`,
     },
   });
 
   return response.status == 200;
-};
+}
+
+export const deactivateAutomation = async (automationUrl: string, secret: string) => {
+  const response = await axios.post(automationUrl, {}, {
+    headers: {
+      'Authorization': `Bearer ${secret}`,
+    },
+  });
+
+  return response.status == 200;
+}
+
+export const deleteAutomation = async (automationUrl: string, secret: string) => {
+  const response = await axios.delete(automationUrl, {
+    headers: {
+      'Authorization': `Bearer ${secret}`,
+    },
+  });
+
+  return response.status == 200;
+}
 
 export const deleteImage = async (imageUrl: string, secret: string) => {
   const response = await axios.delete(imageUrl, {
     headers: {
-      Authorization: `Bearer ${secret}`,
+      'Authorization': `Bearer ${secret}`,
     },
   });
 
